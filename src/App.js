27278883bbedf966f45458431b0281ecf6ca01ba
karenchang-dev <karenchang.dev@gmail.com>
2021-01-16 11:35:29 +1100

    update music bank

--- src/App.js
+++ src/App.js
@@ -6,63 +6,6 @@ const Bank1 = [
   {
     keyCode: 81,
     keyTrigger: "Q",
-    id: "Heater-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-  },
-  {
-    keyCode: 87,
-    keyTrigger: "W",
-    id: "Heater-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-  },
-  {
-    keyCode: 69,
-    keyTrigger: "E",
-    id: "Heater-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-  },
-  {
-    keyCode: 65,
-    keyTrigger: "A",
-    id: "Heater-4",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-  },
-  {
-    keyCode: 83,
-    keyTrigger: "S",
-    id: "Clap",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-  },
-  {
-    keyCode: 68,
-    keyTrigger: "D",
-    id: "Open-HH2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-  },
-  {
-    keyCode: 90,
-    keyTrigger: "Z",
-    id: "Kick-n'-Hat",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-  },
-  {
-    keyCode: 88,
-    keyTrigger: "X",
-    id: "Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-  },
-  {
-    keyCode: 67,
-    keyTrigger: "C",
-    id: "Closed-HH1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-  }
-];
-
-const Bank2 = [
-  {
-    keyCode: 81,
-    keyTrigger: "Q",
     id: "Chord-1",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
   },
@@ -113,6 +56,63 @@ const Bank2 = [
     keyTrigger: "C",
     id: "Snare",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+  }
+];
+
+const Bank2 = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Open-HH2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Closed-HH1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
   }
 ];
 
